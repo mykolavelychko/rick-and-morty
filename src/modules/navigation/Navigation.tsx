f29d2da52af795e8a0c98fca86005132d64a46f6
Nicolai Velicico <nicolai.velicico@finqle.com>
2024-10-06 16:32:53 +0200

    feat(navigation): add styles

--- src/modules/navigation/Navigation.tsx
+++ src/modules/navigation/Navigation.tsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 const NavBar = styled.nav`
-  background-color: #333;
+  background-color: #1e272e;
   padding: 10px;
+  display: flex;
+  font-family: 'Comic Sans MS', 'Comic Sans', cursive;
 `;
 
-const NavLink = styled(Link)`
-  color: white;
+const NavLink = styled(Link)<{ active: boolean }>`
+  color: ${(props) => (props.active ? "#ff9f43" : "white")};
   margin: 0 10px;
   text-decoration: none;
+  font-weight: ${(props) => (props.active ? "bold" : "normal")};
 
   &:hover {
     text-decoration: underline;
@@ -17,11 +20,12 @@ const NavLink = styled(Link)`
 `;
 
 const Navigation = () => {
+  const location = useLocation();
   return (
     <NavBar>
-      <NavLink to="/">Characters</NavLink>
-      <NavLink to="/locations">Locations</NavLink>
-      <NavLink to="/episodes">Episodes</NavLink>
+      <NavLink to="/" active={location.pathname === "/"}>Characters</NavLink>
+      <NavLink to="/locations" active={location.pathname === "/locations"}>Locations</NavLink>
+      <NavLink to="/episodes" active={location.pathname === "/episodes"}>Episodes</NavLink>
     </NavBar>
   );
 };
